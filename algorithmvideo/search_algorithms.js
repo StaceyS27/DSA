908ba0cc1f7ea53e89f16a4a3ce8d79fcde1a507
Stacey Sanchez <staceysanchez27@gmail.com>
2024-01-14 13:00:55 -0500

    recursive binary search

--- algorithmvideo/search_algorithms.js
+++ algorithmvideo/search_algorithms.js
@@ -46,3 +46,26 @@ console.log(binarySearch([-5, 2, 4, 6, 10], 10))
 //Big-O = O(logn) - the number of times function executeed does increase with an increased length of array, but not linear
 
 //____________________Recursive Binary Search_______________________
+
+function recursiveBinarySearch(arr, target) {
+    return search(arr, target, 0, arr.length - 1);
+};
+
+
+function search(arr, target, leftIndex, rightIndex) {
+    if(leftIndex > rightIndex) {
+        return -1;
+    };
+
+    let middleIndex = Math.floor((leftIndex + rightIndex)/2)
+    
+    if(target === arr[middleIndex]) {
+        return middleIndex;
+    };
+
+    if(target < arr[middleIndex]) {
+        return search(arr, target, leftIndex, middleIndex - 1)
+    } else {
+        return search(arr, target, middleIndex + 1, rightIndex)
+    }
+};
