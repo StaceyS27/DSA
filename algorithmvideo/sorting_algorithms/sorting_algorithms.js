dee8ec2f5e603da6af2a14cd4271657316e81f70
Stacey Sanchez <staceysanchez27@gmail.com>
2024-01-15 19:18:42 -0500

    quick sort

--- algorithmvideo/sorting_algorithms/sorting_algorithms.js
+++ algorithmvideo/sorting_algorithms/sorting_algorithms.js
@@ -57,3 +57,35 @@ console.log(array);
 //Big-O = O(n^2) - quadratic time complexity 
 
 //___________________________Quick Sort_______________________________
+
+//takes the last number as a pivot to compare and seperate into left and right array
+//so dont need to traverse the entire array, just up to right before the last element
+//recursively performs quickSort function until base case is met - when array has 1 or 0 elements, return the same array
+//when all subarrays sorted, perform spread operator on them and add all the individual elements into a larger array
+
+function quickSort(array2) {
+    if(array2.length < 2) {
+        return array2
+    };
+
+    let pivot = array2[array2.length - 1]
+    let left = []
+    let right = []
+
+    for(i=0; i<array2.length-1; i++) {
+        if(array2[i] < pivot) {
+            left.push(array2[i])
+        } else {
+            right.push(array2[i])
+        }
+    }
+    
+    return [...quickSort(left), pivot, ...quickSort(right)]
+};
+
+const array2 = [8, 20, -2, 4, 6]
+console.log(quickSort(array2));
+
+//Worst case - O(n^2) when array is already sorted bc iterating with both for loop and recursively 
+//Avg case - O(nlogn) with every recursion, the size of array that reenters the quickSort function decreases
+//but also * n because of the for loop iterating through the whole array 
