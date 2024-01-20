3efed688192a3d466837a513ae6310fe3447a46b
Stacey Sanchez <staceysanchez27@gmail.com>
2024-01-20 13:45:38 -0500

    fix folders

---
+++ data_structures/index.js
@@ -0,0 +1,19 @@
+// data structure: the way to store and organize data so that it can be used/accessed efficiently.
+// also a collection of data values, the relationships b/w them and the functions and operations that can applied to them
+
+// data structures help solve problems in a more efficient way both in terms of time and memory. 
+
+// built in data structures in javascript
+// 1. arrays
+// 2. objects
+// 3. sets
+// 4. maps
+
+// will use built in data structures to build custom data structures
+// 1. stacks
+// 2. queues
+// 3. circular queues
+// 4. linked lists
+// 5. hash tables
+// 6. trees
+// 7. graphs 
