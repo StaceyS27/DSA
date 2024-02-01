acbe7e8e5aafb63809586188b793300542a3ddee
Stacey Sanchez <staceysanchez27@gmail.com>
2024-02-01 12:50:29 -0500

    hashtable overview

---
+++ data_structures/custom_datastructures/hash_table/overview.js
@@ -0,0 +1,21 @@
+// Hash Tables
+
+// hash table (also known as hash map) is a data structure used to store key-value pairs
+// when you have a key, can allow for easy lookup of a value
+// JS object also stores key value pairs
+    // it is a special implementation of hash table 
+    // object class has its own keys so keys that you add may conflict with those present
+// maps were introduced 2015 to resolve that issue | has no inherit keys 
+
+// stores key value pairs in a fix sized array
+// hash tables undergo hashing function to convert string key to numerical index
+// value is then stored
+
+// supports 3 main operations:
+    // 1. set | store key-value pair
+    // 2. get | retrieve a value given its key
+    // 3. remove | delete key value pair 
+
+// hash tables are implemented where constant time lookup and insertion are required
+    // database indexing - used to increase speed of retrieval of data from database by creating indices at specific columns 
+    // caches 
