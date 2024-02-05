f6c8c5d707f4196681a5e6070141e4a50654e635
Stacey Sanchez <staceysanchez27@gmail.com>
2024-02-04 20:17:34 -0500

    insert methods created for both an empty bst and non empty one

--- data_structures/custom_datastructures/binarytree/bst_class.js
+++ data_structures/custom_datastructures/binarytree/bst_class.js
@@ -20,6 +20,33 @@ class BinarySearchTree {            // when a new instance of the BST class is c
     isEmpty() {                         // method to check if binary search tree is empty 
         return this.root === null       // returns true if root node points to null, indicating there are no nodes in BST = empty 
     }
+
+    insert(value) {                                 // method to insert new node to bst. when calling the method, need to pass in value for new node to be created 
+        const newNode = new Node(value)             // start by creating a new instance of the node class, w/  L & R pointers pointing to null
+        if(this.isEmpty()) {
+            this.root = newNode                     // if the bst is empty, the new node becomes the root of the bst 
+        } else {
+            this.insertNode(this.root, newNode)     // else, if not empty, will can on the method insertNode defined below (which accepts the root and new node as arguments )
+        }
+    }
+
+    insertNode(root, newNode) {                         // method to insert a new node is bst is not empty. initially the new node is compared to the root node of the bst,
+        if(newNode.value < root.value) {                // but as the function is called recursively and 'comes back to the top', the new root used to compared is the 'root' of the subtree in the bst 
+            if(root.left === null) {                    // if the new node's value is less than the root, and there is not left child, new node placed as the new left child 
+                root.left = newNode
+            } else {                                    // if there is a left child, the method is called again with the left child being called again as the new root (back to the top)
+                this.insertNode(root.left, newNode)
+            }
+        } else {                                            // if the value of the new node is greater then that of the root ...
+            if(root.right === null) {                       // if the root doesnt have a right child, place new node as R child 
+                root.right = newNode
+            } else {                                        // if the root does have a R child, recursively call method again with R child as the new root and comparing the new node to it (back to the top)
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
+
 }
 
 //__________________________________________________________________________________________________
